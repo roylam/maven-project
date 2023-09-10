0deb6eef121e939f9ea8eaf0d4cf8acb23df604e
Roy Lam <roylam@RoydeMacBook-Air.local>
2023-09-10 23:01:43 +0800

    add package info and java doc in person 2

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -13,11 +13,10 @@ public class Person {
   }
 
   /**
-   * 
    * @param someone
    * @return
    */
-  public String person(final String someone) {
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
