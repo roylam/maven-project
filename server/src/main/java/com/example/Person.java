f9ca141bbb3521fdb419d1d5826a346981dac3f6
Roy Lam <roylam@RoydeMacBook-Air.local>
2023-09-10 22:43:51 +0800

    edit comment and final type

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,8 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  //Add javadoc comment
+  public String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
